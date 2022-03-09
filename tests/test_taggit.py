005565b606945e6ab5a5aa1f928b35143856aaba
Grant Savage <66881605+savageGrant@users.noreply.github.com>
2022-03-08 16:00:31 -0800

    updates test_taggit.py

--- tests/test_taggit.py
+++ tests/test_taggit.py
@@ -32,3 +32,15 @@ def test_create_tag_from_tuple():
     assert taggit.Tag.from_tuple(("tag", "Red")) == taggit.Tag("tag", "Red")
     assert taggit.Tag.from_tuple(("tag", "Blue")) == taggit.Tag("tag", 4)
     assert taggit.Tag.from_tuple(("tag",)) == taggit.Tag("tag", 0)
+
+
+def test_remove_tag():
+    # Tests for Mac OSx
+    taggit._OPERATING_SYSTEM = 'Darwin'
+    taggit.remove_all_tags('test_file.txt')
+    taggit.add_tag(("tag", "red"), 'test_file.txt')
+    taggit.add_tag(("tag", "blue"), 'test_file.txt')
+    assert taggit.get_tags('test_file.txt') == \
+           [taggit.Tag("tag", "red"), taggit.Tag("tag", "blue")]
+    taggit.remove_tag(('tag', 'red'), 'test_file.txt')
+    assert taggit.get_tags('test_file.txt') == [taggit.Tag("tag", "blue")]
